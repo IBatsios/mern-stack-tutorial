9eb655a3a50181e4170925a15ce1916fddd2576a
ibatsios <ioannisbatsios@gmail.com>
2020-02-29 00:35:58 -0500

    added routes. Fixed connection to db

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
 // Connect Database
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
